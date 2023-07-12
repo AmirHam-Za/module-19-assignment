503509ac3554377339690332cfb67ceac0b35aa1
AmirHam-Za <hamza.com.bd@gmail.com>
2023-07-13 03:08:39 +0600

    uploaded

---
+++ assignment-19/resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-blue-200 text-gray-600 body-font shadow-md">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        
+        <img src="{{URL::asset('/logo ostad.png')}}" alt="profile Pic" height="120" width="120">
+        
+        <a href="{{ route('home') }}"
+            class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-yellow-600 lg:items-center lg:justify-center mb-4 md:mb-0">
+            <span class="ml-3 text-xl uppercase">MY Blogs</span>
+        </a>
+    </div>
+</header>
